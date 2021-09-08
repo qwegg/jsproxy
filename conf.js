b100e8df9e26fd4d8a98cb016337eea5d92b8eb4
qwegg <37839352+qwegg@users.noreply.github.com>
2021-09-08 06:25:49 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '131',
+  ver: '120',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -21,10 +21,16 @@ jsproxy_config({
         'node-aliyun-hk-2.etherdream.com:8443': 1,
       }
     },
+    'demo-sg': {
+      label: '演示服务-新加坡节点',
+      lines: {
+        'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
    'Seoul': {
       label: '龟壳云-首尔',
       lines: {
-        'c9.eud.eu.org:8443': 1,
+        'm.bocc.online:8443': 1,
       },
     },
     'mysite': {
@@ -33,7 +39,14 @@ jsproxy_config({
         [location.host]: 1,
       }
     },
-
+    // 该节点用于加载大体积的静态资源
+    'cfworker': {
+      label: '',
+      hidden: true,
+      lines: {
+        'node-cfworker-2.etherdream.com': 1,
+      }
+    }
   },
 
   /**
@@ -51,7 +64,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/qwegg/jsproxy@master/assets/',
+  //assets_cdn: 'https://cdn.jsdelivr.net/gh/qwegg/jsproxy@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   //assets_cdn: 'assets/',
