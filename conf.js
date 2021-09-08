fcdab2944a2725297e80150bc23621c673beffdd
qwegg <37839352+qwegg@users.noreply.github.com>
2021-09-08 06:43:35 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '130',
+  ver: '132',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -34,7 +34,7 @@ jsproxy_config({
       }
     },
     // 该节点用于加载大体积的静态资源
-    'CF-Worker': {
+    'cf-worker': {
       label: 'cfworker',
       //hidden: true,
       lines: {
@@ -54,7 +54,7 @@ jsproxy_config({
   /**
    * 加速节点
    */
-  node_acc: 'CF-Worker',
+  node_acc: 'cf-worker',
 
   /**
    * 静态资源 CDN 地址
